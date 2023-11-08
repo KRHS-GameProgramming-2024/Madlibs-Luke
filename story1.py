7e1f7d4845852ab9e963f53b8d2ed6a22c32a08b
Luke <luke.mann@kearsarge.org>
2023-11-08 08:34:29 -0500

    I made it so that it actually works now

--- story1.py
+++ story1.py
@@ -4,12 +4,13 @@ def story1(debug = False):
     if debug: print("story1 function")
     
     print("\n")
-    Place1 = getWord("Enter place name here", debug)
-
-    out = "\n"
-    out += "one day I was at " + Place1
+    Place1 = input("Enter place name here:")
+    friendName1 = input("Enter friend name here:")
+   
+    print("\n")
+    print("one day I was at " + Place1)
+    print("I was with a friend named " + friendName1)
     
     
     
-    
-    return out
+   
