7151814b02385d3e0979a695f5d49d313abce6b5
Luke <luke.mann@kearsarge.org>
2023-11-27 09:05:38 -0500

    added more to the second story

--- story2.py
+++ story2.py
@@ -4,6 +4,16 @@ def story2(debug = False):
     if debug: print("story2 function")
 
 	print("\n")
-	
+	yourName = input("Enter a name here:")
+	job1 = input("Enter a job, profesion, or thing you can be here:")
+	problem1 = input("Enter something you do in your spare time here:")
+	verb1 = input("Enter a verb here:")
 	
 	print("\n")
+	print("my name is " + yourName)
+	print("and I am a " + job1)
+	print("I have a problem, while I'm at work I constintly have the urge to " + problem1)
+	print("it bothers my coworkers, and makes my boss " + verb1)
+	
+	
+	
